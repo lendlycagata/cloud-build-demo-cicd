85b8166166ec78744c467c0faa73482245d7064f
lendly-telus-com <lendly.cagata@telus.com>
2025-07-28 09:40:05 +0800

    fix: 12

--- hello-world-gke/app.py
+++ hello-world-gke/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World from GKE Autopilot v1.0.10!"
+    return "Hello, World from GKE Autopilot v1.0.12!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
